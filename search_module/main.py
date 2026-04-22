34de123a6b1fbe251a1a3d9258efacc25f4d1cc2
Artemdjdj <artembasuraa@gmail.com>
2026-04-22 22:34:14 +0300

    feat: add data.json and delete unused filter func

--- search_module/main.py
+++ search_module/main.py
@@ -14,18 +14,6 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 model = SentenceTransformer(PATH_TO_MODEL)
 
-if not os.path.exists('data.json'):
-    data = [
-        {"id": "1", "text": "C++ backend разработка API серверов", "metadata": {"cat": "tech"}},
-        {"id": "2", "text": "Python ChromaDB векторный поиск", "metadata": {"cat": "tech"}},
-        {"id": "3", "text": "Machine Learning алгоритмы KNN", "metadata": {"cat": "ml"}},
-        {"id": "4", "text": "PostgreSQL оптимизация запросов", "metadata": {"cat": "db"}},
-        {"id": "5", "text": "Программирование на C++ для высоконагруженных систем", "metadata": {"cat": "tech"}},
-        {"id": "6", "text": "Современное программирование: Python и автоматизация", "metadata": {"cat": "tech"}}
-    ]
-    with open('data.json', 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=2)
-
 client = chromadb.PersistentClient(path=PATH_TO_CHROMA_DB)
 collection = client.get_or_create_collection(name="my_vectors")
 
@@ -39,13 +27,6 @@ metadatas = [item['metadata'] for item in data]
 
 collection.add(embeddings=embeddings, documents=texts, metadatas=metadatas, ids=ids)
 
-def apply_filter(query):
-    query_lower = query.lower()
-    if "c++" in query_lower or "python" in query_lower:
-        return {"cat": "tech"}
-
-    return None
-
 while True:
     query = input("\nВведите запрос (или 'exit'): ").strip()
     if query.lower() == 'exit':
@@ -55,7 +36,6 @@ while True:
     results = collection.query(
         query_embeddings=query_emb,
         n_results=4,
-        where=apply_filter(query)
     )
 
     docs = results['documents'][0]
@@ -65,6 +45,6 @@ while True:
 
 
     for i, (doc, dist, meta) in enumerate(zip(docs, dists, metas)):
-        if i >= 3:
+        if i >= 4:
             break
         print(f"  {i + 1}. '{doc}' (дистанция: {dist:.3f}) [{meta['cat']}]")
