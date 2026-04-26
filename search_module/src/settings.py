4709a2922649db94f5552b440df244428314b21b
Artemdjdj <artembasuraa@gmail.com>
2026-04-26 15:47:23 +0300

    feat: add logger to main.py

--- search_module/src/settings.py
+++ search_module/src/settings.py
@@ -4,3 +4,4 @@ BASE_DIR = Path(__file__).parent.parent
 PATH_TO_TEST_JSON = BASE_DIR / "data.json"
 PATH_TO_MODEL = BASE_DIR / "models" / "all-MiniLM-L6-v2"
 PATH_TO_CHROMA_DB = BASE_DIR / "chroma_db"
+LOG_FILE = BASE_DIR / "logs" / "search_module.log"
