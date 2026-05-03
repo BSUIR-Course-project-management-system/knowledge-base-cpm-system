af3649ec326e5b571c05dcfcc596b2f7fd6d89fa
Artemdjdj <artembasuraa@gmail.com>
2026-05-03 17:32:53 +0300

    fix: change path to data

--- search_module/src/settings.py
+++ search_module/src/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 BASE_DIR = Path(__file__).parent.parent
-PATH_TO_TEST_JSON = BASE_DIR / "data.json"
+PATH_TO_TEST_JSON = BASE_DIR / "data" /"data.json"
 PATH_TO_MODEL = BASE_DIR / "models" / "all-MiniLM-L6-v2"
 PATH_TO_CHROMA_DB = BASE_DIR / "chroma_db"
 MAIN_LOG_FILE = BASE_DIR / "logs" / "search_module_main.log"
