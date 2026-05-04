75b90e828b585592f16738e7c12085c69761dfcc
Artemdjdj <artembasuraa@gmail.com>
2026-05-04 20:11:37 +0300

    feat[main]: add setup.sh

---
+++ scripts/setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Создание виртуального окружения"
+python3 -m venv .venv
+echo "Виртуальное окружение успешно создано"
+
+echo "Активация виртуального окружения"
+source .venv/bin/activate
+pip install --upgrade pip
+echo "Виртуальное окружение активировано"
+
+echo "Установка необходимых библиотек"
+pip install -r requirements.txt
+echo "Все необходимые библиотеки установлены"
+
+echo "Установка ситемы поиска"
+
+chmod +x search_module/scripts/setup.sh
+
+./search_module/scripts/setup.sh
+
+echo "Система поиска успешно установлена!"
+
+
+echo "Система управления курсовыми и дипломными проектами готова к запуску!"
