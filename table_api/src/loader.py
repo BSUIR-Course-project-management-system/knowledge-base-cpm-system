3902d9d0402753b4824252f9062a76f18d7cdfad
Frostnout <dima.burbas@gmail.com>
2026-05-04 09:53:59 +0300

    feat[tab_api]: add loader init method

--- table_api/src/loader.py
+++ table_api/src/loader.py
@@ -1,5 +1,8 @@
 from abc import ABC, abstractmethod
+from logger.logger import Logger
 import json
+
+LOG_FILE = "table_api/logs/loader.log"
 
 
 class ILoader(ABC):
@@ -9,11 +12,17 @@ class ILoader(ABC):
 
 
 class JsonLoader(ILoader):
+    def __init__(self, encoding: str = "utf-8"):
+        self._encoding = encoding
+        self._logger = Logger(LOG_FILE, level="INFO")
+
     def load(self, file_path: str) -> dict:
         try:
-            with open(file_path, "r", encoding="utf-8") as f:
+            with open(file_path, "r", encoding=self._encoding) as f:
+                # self._logger.info(f"Попытка загрузки из файла {file_path}")
                 result = json.load(f)
+                # self._logger.info(f"Данные загружены из файла {file_path}")
                 return result
         except FileNotFoundError:
-            # logging.error("Файл для загрузки не найден")
+            self._logger.error("Файл для загрузки не найден")
             return None
